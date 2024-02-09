d881a9b5621234cf70b4b1d90945fb2511306956
Riazul Islam <riazul5207@gmail.com>
2024-02-10 00:00:27 +0600

    header ui updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,17 +15,17 @@ const Header = () => {
                     <span className="text-[1.1rem] font-[500] me-[0.3rem]">Users: 100 / 100</span>
                 </div>
                 
-                <div className="ps-[0.12rem] flex items-center justify-center bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 rounded-lg overflow-hidden">          
-                    <input name="searchText" className="pl-[1rem] text-[1.1rem] text-[#fff] h-[41px] w-[360px] bg-[#000] border-none outline-none rounded-l-lg" type="text" placeholder="Search users..."/>
+                <div className="flex items-center justify-center rounded-lg overflow-hidden">          
+                    <input name="searchText" className="pl-[1rem] text-[1.1rem] text-[#fff] h-[44px] w-[360px] bg-[#000] border-[2px] border-r-0 border-emerald-500 outline-none rounded-l-lg" type="text" placeholder="Search users..."/>
                     
-                    <div className="text-[1.1rem] text-[#fff] flex items-center justify-center h-[44px] w-[52px] cursor-pointer">
+                    <div className="text-[1.1rem] text-[#fff] flex items-center justify-center h-[44px] w-[52px] bg-gradient-to-r from-emerald-500 to-indigo-500 cursor-pointer">
                         <BiSearchAlt className="text-[1.6rem]"/>
                     </div>
                 </div>
 
-                <div className="pe-[0.1rem] flex items-center bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 outline-none rounded-lg overflow-hidden">
-                    <p className="px-[0.8rem] py-[0.55rem] text-[1.1rem] font-[500]">Sort by</p>
-                    <select name="searchCategory" className="px-[0.6rem] py-[0.55rem] text-[#fff] text-[1.1rem] bg-[#000] rounded-r-lg outline-none">
+                <div className="flex items-center outline-none rounded-lg overflow-hidden">
+                    <p className="px-[0.8rem] flex items-center justify-center h-[44px] text-[1.1rem] font-[500] bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500">Sort by</p>
+                    <select name="searchCategory" className="px-[0.6rem] h-[44px] text-[#fff] text-[1.1rem] bg-[#000] border-[2px] border-emerald-500 rounded-r-lg outline-none">
                         <option value="name">Name</option>
                         <option value="email">Email</option>
                         <option value="company">Company Name</option>
