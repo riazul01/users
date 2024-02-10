30a7b9263274a37b3fb0e17d22c1063741f4be92
Riazul Islam <riazul5207@gmail.com>
2024-02-10 16:23:22 +0600

    pagination ui updated

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -53,7 +53,8 @@ const Pagination = () => {
 
     return (
         <div className="flex items-center justify-between w-full h-auto">
-            <ul className="flex items-center justify-start gap-[1rem]">
+            {/* pagination */}
+            <div className="flex items-center justify-start gap-[1rem]">
                 <button className="flex items-center justify-center h-[2.6rem] w-[3rem] bg-[#222] rounded-lg" onClick={prevPage}><MdKeyboardDoubleArrowLeft className="text-[1.3rem]"/></button>
                 {(currentPage > 2) && <button className="text-[1.1rem] h-[2.6rem] w-[3rem] bg-[#222] rounded-lg" onClick={() => setCurrentPage(1)}>1</button>}
                 {(currentPage > 3) && <button className="text-[1.1rem] h-[2.6rem] w-[3rem] bg-[#222] rounded-lg" onClick={multiPrevPage}>...</button>}
@@ -63,8 +64,9 @@ const Pagination = () => {
                 {(currentPage + 2 < totalPages) && <button className="text-[1.1rem] h-[2.6rem] w-[3rem] bg-[#222] rounded-lg" onClick={multiNextPage}>...</button>}
                 {(currentPage + 1 < totalPages) && <button className="text-[1.1rem] h-[2.6rem] w-[3rem] bg-[#222] rounded-lg" onClick={() => setCurrentPage(totalPages)}>{totalPages}</button>}                
                 <button className="flex items-center justify-center h-[2.6rem] w-[3rem] bg-[#222] rounded-lg" onClick={nextPage}><MdKeyboardDoubleArrowRight className="text-[1.3rem]"/></button>
-            </ul>
+            </div>
 
+            {/* set limit */}
             <div className="flex items-center outline-none rounded-lg overflow-hidden">
                 <p className="px-[0.8rem] flex items-center justify-center h-[44px] text-[1.1rem] font-[500] bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500">Limit</p>
                 <select value={limit} onChange={handleLimitChange} name="searchCategory" className="px-[0.6rem] h-[44px] text-[#fff] text-[1.1rem] bg-[#000] border-[2px] border-emerald-500 rounded-r-lg outline-none">
