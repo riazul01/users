b755c7c6d9d973439da593b7027374595dc92c51
Riazul Islam <riazul5207@gmail.com>
2024-02-10 22:32:21 +0600

    user card updated

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UserCard = ({ user }) => {
     return (
         <div className="h-auto w-full bg-[#222] rounded-lg overflow-hidden">
+            {/* image */}
             <div className="p-[0.6rem] pb-0 w-full h-[320px]">
                 <img src={user.image} alt={user.firstName} className="w-full h-full object-cover" />
             </div>
             
+            {/* seperator */}
             <div className="w-full h-[0.35rem] bg-gradient-to-r from-indigo-400 via-sky-400 to-emerald-400"></div>
             
+            {/* details */}
             <div className="py-[1rem] flex flex-col items-center w-full h-[260px]">
-                <p className="text-[1.5rem] text-center font-[700] bg-gradient-to-r from-indigo-400 via-sky-400 to-emerald-400 inline-block text-transparent bg-clip-text">{user.firstName} {user.lastName}</p>
+                <Link to={`/users/${user.id}`} className="text-[1.5rem] text-center font-[700] bg-gradient-to-r from-indigo-400 via-sky-400 to-emerald-400 inline-block text-transparent bg-clip-text">{user.firstName} {user.lastName}</Link>
                 <p className="text-[#ddd] text-[1.2rem] text-center font-[500]">{user.email}</p>
                 <p className="text-[#ddd] text-[1.2rem] text-center font-[500]">🏬{user.company.name}</p>
                 <div className="mt-[1rem]">
