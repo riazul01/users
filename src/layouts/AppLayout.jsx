0a1640413bb1c6e78e82bd1d48d56bf43970b688
Riazul Islam <riazul5207@gmail.com>
2024-02-11 21:48:50 +0600

    app layout updated

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AppLayout = ({ children }) => {
     return (
-        <div className="mx-auto px-[0.4rem] py-[3rem] w-full h-full max-w-[1420px] min-h-screen">
+        <div className="mx-auto px-[0.4rem] py-[2rem] lg:py-[3rem] w-full h-full max-w-[1420px] min-h-screen overflow-x-hidden">
             {children}
         </div>
     );
